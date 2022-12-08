679c0de67e890674f881f3feacc6587e6f2c8e8f
TA1137 <muhammad.reza.ft21@mail.umy.ac.id>
2022-12-08 16:42:15 +0700

    Add pop di dalam stacks

--- Exe4_137/Program.cs
+++ Exe4_137/Program.cs
@@ -45,6 +45,12 @@ namespace Exe4_137
             top = fresh;
             Console.WriteLine("\n" + element + " pushed. ");
         }
+
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next; //Make top point to the next node in sequence
+        }
     }
 
 
